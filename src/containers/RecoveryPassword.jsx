d43ecaed226910ff32f953eca708fbcbb68b92dd
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-01-10 15:41:57 -0500

    Add RecoveryPassword component

---
+++ src/containers/RecoveryPassword.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import '../styles/RecoveryPassword.scss';
+
+const RecoveryPassword = () => {
+  return (
+    <div className="login">
+      <div className="form-container">
+        <img src="./assets/logos/coffe-and-code.jpg" alt="logo" className="logo" />
+        <h1 className="title">Email has been send!</h1>
+        <p className="subtitle">
+          Please check your inbox for instructions on how to reset the password
+        </p>
+        <div className="email-image">
+          <img src="./assets/icons/email.svg" alt="email" />
+        </div>
+        <button className="primary-button login-button">Login</button>
+        <p className="resend">
+          <span>Didn't receive the email?</span>
+          <a href="/">Resend</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default RecoveryPassword;
