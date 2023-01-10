e5c256a1e69791f3a7baf62b26da3871492b7519
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-01-10 16:19:56 -0500

    Using appropriate React 18 components for route navigation

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,21 +1,23 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../containers/Layout";
 import Login from "../containers/Login";
 import RecoveryPassword from "../containers/RecoveryPassword";
+import Home from '../pages/Home';
+import NotFound from '../pages/NotFound';
 import "../styles/global.css";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Layout>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="recover-password" component={RecoveryPassword} />
-          <Route component={NotFound} />
-        </Layout>
-      </Switch>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/recovery-password" element={<RecoveryPassword />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 };
