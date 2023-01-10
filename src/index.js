9364365180f92e2caff4fdb629510cf4c6d86a4c
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-01-10 16:19:17 -0500

    Change structure to render with React 18

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import ReactDOM from 'react-dom/client';
+import App from './routes/App';
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('app')
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(
+  <App/>
 );
