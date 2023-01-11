0f140123c0c2441a5b3ca9b7e2d2283afeed8c80
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-01-11 15:45:57 -0500

    Add aliases for products

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,8 @@ module.exports = {
       '@pages': path.resolve(__dirname, 'src/pages/'),
       '@styles': path.resolve(__dirname, 'src/styles/'),
       '@icons': path.resolve(__dirname, 'src/assets/icons/'),
-      '@logos': path.resolve(__dirname, 'src/assets/logos/')
+      '@logos': path.resolve(__dirname, 'src/assets/logos/'),
+      '@products': path.resolve(__dirname, 'src/assets/products/'),
     }
   },
   module: {
