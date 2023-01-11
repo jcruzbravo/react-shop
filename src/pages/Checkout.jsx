cef7fff5ee414ff484e939298d9dad78450dcab9
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-01-11 14:21:22 -0500

    Add pages and their styles

---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import OrderItem from '../components/OrderItem';
+
+const Checkout = () => {
+  return (
+    <div className="Checkout">
+    <div className="Checkout-container">
+      <h1 className="title">My order</h1>
+      <div className="Checkout-content">
+        <div className="order">
+          <p>
+            <span>04.25.21</span>
+            <span>6 articles</span>
+          </p>
+          <p>$560.00</p>
+        </div>
+      </div>
+      <OrderItem/>
+    </div>
+  </div>
+  );
+}
+
+export default Checkout;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from '../components/Header';
+import ProductList from '../containers/ProductList';
 
 const Home = () => {
   return (
-    <div>
-      <h1>React Shop</h1>
-    </div>
+    <>
+      <Header/>
+      <ProductList/>
+    </>
   );
 }
 
