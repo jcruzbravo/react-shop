cb3e887399b4e4f8445cb0b9793c76673b1b95f5
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-01-11 18:11:45 -0500

    Use useState

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import '@styles/ProductItem.scss'
 
 import cupCoffe from '@products/cup-coffe6.jpg';
 import addToCart from '@icons/bt_add_to_cart.svg';
 
 const ProductItem = () => {
+
+  const [cart, setCart] = useState([]);
+
+  const handleCart = () => {
+    setCart([]);
+  }
+
   return (
     <div className="ProductItem">
       <img src={cupCoffe} alt="cup_coffe" className="product-img" />
@@ -13,7 +20,7 @@ const ProductItem = () => {
           <p>$120,00</p>
           <p>Bike</p>
         </div>
-        <figure>
+        <figure onClick={handleCart}>
           <img src={addToCart} alt="add to cart logo" />
         </figure>
       </div>
