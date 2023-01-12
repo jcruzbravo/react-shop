21483f71a28735f4019729ec60e54df2eaa0f55e
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-01-12 10:53:41 -0500

    Use useGetProducts hook to get them

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductItem from '../components/ProductItem';
+import useGetProducts from '@hooks/useGetProducts';
 import '@styles/ProductList.scss';
 
+const API = 'https://api.escuelajs.co/api/v1/products';
+
 const ProductList = () => {
+
+	const products = useGetProducts(API);
 	return (
-		<section className="main-container">
-			<div className="ProductList">
-				<ProductItem />
-			</div>
-		</section>
-	);
+    <section className="main-container">
+      <div className="ProductList">
+        {products.map((product) => (
+          <ProductItem key={product.id} product={product}/>
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default ProductList;
