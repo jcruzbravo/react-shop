9412d73f4f216ff66d151efe382d59577c9e0ba9
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-01-12 10:49:55 -0500

    Change the path using its alias

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './routes/App';
+import App from '@routes/App';
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
