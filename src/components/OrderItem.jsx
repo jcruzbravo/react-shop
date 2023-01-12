9a4c74e13e0185434dc5f87dd94e5371c90eb64a
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-01-12 13:46:11 -0500

    Add function for remove product from shopping cart

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '@context/AppContext'
 import '@styles/OrderItem.scss'
 
 import closeIcon from '@icons/icon_close.png';
 
-const OrderItem = ({product}) => {
+const OrderItem = ({product, indexValue}) => {
+  const { removeFromCart } = useContext(AppContext);
+
+  const handleRemove = (index) => {
+    removeFromCart(index);
+  }
+
   return (
     <div className="OrderItem">
       <figure>
@@ -11,7 +18,7 @@ const OrderItem = ({product}) => {
       </figure>
       <p>{product.title}</p>
       <p>${product.price}</p>
-      <img src={closeIcon} alt="close" />
+      <img src={closeIcon} alt="close" onClick={() => handleRemove(indexValue)}/>
     </div>
   );
 };
