67ea2834a12bf7deb16d6685264fd9f994a0085c
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-01-12 10:52:53 -0500

    Use useContext for productItem

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,27 +1,26 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
+import AppContext from '@context/AppContext';
 import '@styles/ProductItem.scss'
+import addToCartIcon from '@icons/bt_add_to_cart.svg';
 
-import cupCoffe from '@products/cup-coffe6.jpg';
-import addToCart from '@icons/bt_add_to_cart.svg';
+const ProductItem = ({ product }) => {
 
-const ProductItem = () => {
+  const { addToCart } = useContext(AppContext);
 
-  const [cart, setCart] = useState([]);
-
-  const handleCart = () => {
-    setCart([]);
+  const handleCart = (item) => {
+    addToCart(item);
   }
 
   return (
     <div className="ProductItem">
-      <img src={cupCoffe} alt="cup_coffe" className="product-img" />
+      <img src={product.images[0]} alt={product.title} className="product-img" />
       <div className="product-info">
         <div>
-          <p>$120,00</p>
-          <p>Bike</p>
+          <p>${product.price}</p>
+          <p>{product.title}</p>
         </div>
-        <figure onClick={handleCart}>
-          <img src={addToCart} alt="add to cart logo" />
+        <figure onClick={() => handleCart(product)}>
+          <img src={addToCartIcon} alt="add to cart logo" />
         </figure>
       </div>
     </div>
