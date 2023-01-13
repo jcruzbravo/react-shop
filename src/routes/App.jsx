f1503ca7f4aa3583498fd40b13108ffe23a45e2f
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-01-13 14:27:56 -0500

    Deploy

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
 
   return (
     <AppContext.Provider value={initialState}>
-      <BrowserRouter>
+      <BrowserRouter basename="/reactShop">
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
