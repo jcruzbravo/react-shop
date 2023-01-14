d20378c6cd6d529e2a466ece9395b76f1db50a52
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-01-13 21:16:50 -0500

    Add props to close MyOrder modal with arrow

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -68,7 +68,7 @@ const Header = () => {
         </ul>
       </div>
       { toggle && <Menu/> }
-      { toggleOrders && <MyOrder/> }
+      { toggleOrders && <MyOrder toggleOrders={toggleOrders} setToggleOrders={setToggleOrders}/> }
     </nav>
   );
 };
